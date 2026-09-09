8df66532efa324e58ac9010a4aa297bf9e33936c
REDDY HASINI DURGA <reddy.hasinidurga2307@gmail.com>
2026-09-09 09:56:18 +0530

    Add farmer registration API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from backend.app.modules.farmer.router import router as farmer_router
+from backend.app.db.database import Base, engine
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="GrainFlow Smart Procurement Platform",
     description="AI-powered smart procurement and queue management system",
     version="1.0.0"
 )
+app.include_router(farmer_router)
 
 @app.get("/")
 def root():
